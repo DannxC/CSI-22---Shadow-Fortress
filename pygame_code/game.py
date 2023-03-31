2d8b1b5eebe21c61709ed309fc1d8cdaddb2f965
Daniel Cavassani <101767386+DannxC@users.noreply.github.com>
2023-03-31 02:37:23 -0300

    First Game Body definitions!

--- pygame_code/game.py
+++ pygame_code/game.py
@@ -1,4 +1,25 @@
-'''
-This file will contain the main game loop and all the functions 
-and objects you need to run the game.
-'''
+import pygame, sys
+
+# Setup
+pygame.init()
+screen_width = 1200
+screen_height = 700
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+
+# Main game loop
+while True:
+    # Checking events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    screen.fill('black')
+    
+    # Update game objects and perform any game logic
+
+
+    # Update Screen
+    pygame.display.update()
+    clock.tick(60)
